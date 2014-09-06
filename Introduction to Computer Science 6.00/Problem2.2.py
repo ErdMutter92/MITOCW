c968ab1fa207e117e8438d5d6a71ad61ae52c709
ErdMutter92 <alexis@bleauweb.net>
2014-09-06 00:09:44 -0400

    Simplification of PS1P2
    Simplified the equation used to figure out the balance and payment to
    pay off a credit card with in 1 year.

--- Introduction to Computer Science 6.00/Problem2.2.py
+++ Introduction to Computer Science 6.00/Problem2.2.py
@@ -13,9 +13,12 @@ while 1:
     payment = 10 * count
     outstanding_balance = outstanding_balance_save
     for i in range(1, 13):
-        interest_paid = round(interest_rate/12.0 * outstanding_balance, 2)
-        principle_paid = round(payment - interest_paid, 2)
-        outstanding_balance = round(outstanding_balance - principle_paid, 2)
+        ## Removed un-necisary bulk from the program.
+        #interest_paid = round(interest_rate/12.0 * outstanding_balance, 2)
+        #principle_paid = round(payment - interest_paid, 2)
+        #outstanding_balance = round(outstanding_balance - principle_paid, 2)
+        monthly_interest_rate = interest_rate / 12.0
+        outstanding_balance = outstanding_balance * (1 + monthly_interest_rate) - payment
         if outstanding_balance < 0:
             months = i
             break
