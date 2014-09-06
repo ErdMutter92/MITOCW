1e4dd33d37cfe0257a2e0c73b607eb1af8a34852
ErdMutter92 <alexis@bleauweb.net>
2014-09-06 00:06:13 -0400

    Problem 2 of Problem set 1
    Finished problem 2 of problem set 1… Had to fix an issue where range is
    range(x,y) = (x, x+1, … , y-1)

---
+++ Introduction to Computer Science 6.00/Problem2.2.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+## Paying Debt Off In a Year
+
+outstanding_balance_save = float(raw_input("Enter the outstanding balance on your credit card: "))
+interest_rate = float(raw_input("Enter the annual credit card interest rate as a decimal: "))
+
+print "RESULT"
+
+count = 0
+while 1:
+    count = count + 1
+    payment = 10 * count
+    outstanding_balance = outstanding_balance_save
+    for i in range(1, 13):
+        interest_paid = round(interest_rate/12.0 * outstanding_balance, 2)
+        principle_paid = round(payment - interest_paid, 2)
+        outstanding_balance = round(outstanding_balance - principle_paid, 2)
+        if outstanding_balance < 0:
+            months = i
+            break
+    if outstanding_balance < 0:
+        break
+
+print "Monthly payment to pay of debt in 1 year: ", payment
+print "Number of months needed: ", months
+print "Balance: ", round(outstanding_balance, 2)
