19a0b1c87a825195740af4b1740459a37f12eeee
ErdMutter92 <alexis@bleauweb.net>
2014-09-07 23:10:10 -0400

    working on problem set 1 problem 3

--- Introduction to Computer Science 6.00/Problem2.2.py
+++ Introduction to Computer Science 6.00/Problem2.2.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-## Paying Debt Off In a Year
+## Paying Debt Off In a Year given a outstanding balance and interest rate.
 
 outstanding_balance_save = float(raw_input("Enter the outstanding balance on your credit card: "))
 interest_rate = float(raw_input("Enter the annual credit card interest rate as a decimal: "))
@@ -13,10 +13,6 @@ while 1:
     payment = 10 * count
     outstanding_balance = outstanding_balance_save
     for i in range(1, 13):
-        ## Removed un-necisary bulk from the program.
-        #interest_paid = round(interest_rate/12.0 * outstanding_balance, 2)
-        #principle_paid = round(payment - interest_paid, 2)
-        #outstanding_balance = round(outstanding_balance - principle_paid, 2)
         monthly_interest_rate = interest_rate / 12.0
         outstanding_balance = outstanding_balance * (1 + monthly_interest_rate) - payment
         if outstanding_balance < 0:
---
+++ Introduction to Computer Science 6.00/Problem2.3.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+## Using bisection search to figure out
+## lowist possible mayment to pay off
+## a credit card in a single year.
+
+##Balance has an underscore to differentiate
+## it from the testig balance value.
+balance_ = 320000
+interest = 0.2
+epsilon = 0.01
+
+## lowest possible payment needed per month,
+## figuring if you did not have to pay any
+## interest on the credit card/loan.
+low = balance_ / 12.0
+
+## highest possible payment needed per month,
+## figuring if you did not make a payment until
+## the vary last day after all interest is
+## compounded monthly.
+high = (balance_ * (1 + (interest / 12.0)) ** 12.0) / 12.0
+
+while 1:
+    ## Reset balance back to user input value
+    balance = balance_
+    ## the average of the highist and lowist possible payment.
+    middle = (low + high) / 2.0
+    
+    interest_paid = 0
+    
+    for months in range(1, 13):
+        interest_ = interest/12.0*balance
+        interest_paid += interest_
+        principal_paid = middle - interest_
+        balance = balance - principal_paid
+        
+    if balance < 0:
+        high = middle
+    elif balance > 0:
+        low = middle
+    elif balance < epsilon:
+        break
+        
+    print balance, round(middle, 2)
