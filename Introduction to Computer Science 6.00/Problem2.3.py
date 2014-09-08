4c005ec16124a74d6fba8a4baa5325a38d72fcc4
ErdMutter92 <alexis@bleauweb.net>
2014-09-08 00:24:50 -0400

    Working on problem set 1 problem 3
    changed interest to have a saved value for the user generated input.
    Also removed useless interest_paid variable (dead code).

--- Introduction to Computer Science 6.00/Problem2.3.py
+++ Introduction to Computer Science 6.00/Problem2.3.py
@@ -7,7 +7,7 @@
 ##Balance has an underscore to differentiate
 ## it from the testig balance value.
 balance_ = 320000
-interest = 0.2
+interest_ = 0.2
 epsilon = 0.01
 
 ## lowest possible payment needed per month,
@@ -19,7 +19,7 @@ low = balance_ / 12.0
 ## figuring if you did not make a payment until
 ## the vary last day after all interest is
 ## compounded monthly.
-high = (balance_ * (1 + (interest / 12.0)) ** 12.0) / 12.0
+high = (balance_ * (1 + (interest_ / 12.0)) ** 12.0) / 12.0
 
 while 1:
     ## Reset balance back to user input value
@@ -27,14 +27,12 @@ while 1:
     ## the average of the highist and lowist possible payment.
     middle = (low + high) / 2.0
     
-    interest_paid = 0
-    
     for months in range(1, 13):
-        interest_ = interest/12.0*balance
-        interest_paid += interest_
-        principal_paid = middle - interest_
+        interest = interest_/12.0*balance
+        principal_paid = middle - interest
         balance = balance - principal_paid
         
+    
     if balance < epsilon:
         high = middle
     elif balance > epsilon:
